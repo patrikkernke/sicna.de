9aab32fa39f5e70693cb83063fcbe938f700c5ac
Patrick Kernke <patrick@kernke.net>
2015-09-28 14:40:05 +0200

    Style navbar-component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,35 +3,52 @@
 <head>
     <meta charset="UTF-8">
     <title>SICNA.de - Attraktive Folienlösungen (Beschriftung, Lackschutz, Sonnenschutz & Wrapping)</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+
 </head>
-<body>
+<body id="csstyle">
     
     <nav class="navbar">
         
         <div class="navbar__brand">
-            <img src="/images/brand-logo." alt="SICNA.de Logo">
+            <img src="/images/sicna-logo_nav.png" alt="SICNA.de Logo">
         </div>
         
         <div class="navbar__nav-primary">
             <ul class="navbar__nav-primary__menue">
-                <li><a href="#dienstleistungen">Dienstleistungen</a></li> 
+                <li>
+                    <a href="#dienstleistungen">Dienstleistungen</a>
+                    <ul class="navbar__nav-primary__menue__submenue">
+                        <li><a href="#">Car-Design</a></li>
+                        <li><a href="#">Car-Wrapping</a></li>
+                        <li><a href="#">Fahrzeugbeschriftung</a></li>
+                        <li><a href="#">Lackschutz KFZ</a></li>
+                        <li><a href="#">Lackschutz Fahrrad</a></li>
+                        <li><a href="#">Sonnenschutz KFZ</a></li>
+                        <li><a href="#">Sonnenschutz Gebäude</a></li>
+                        <li><a href="#">Werbebeschriftung</a></li>
+                    </ul>
+                </li> 
                 <li><a href="#ueberuns">Über uns</a></li> 
-                <li><a href="#kontakt">Kontakt uns</a></li> 
+                <li>
+                    <a href="#kontakt">Kontakt</a>
+                    <ul class="navbar__nav-primary__menue__submenue">
+                        <li><a href="#"><i class="fa fa-file-text-o"></i> Anfrage</a></li>
+                        <li><a href="#"><i class="fa fa-envelope-o"></i>Mail</a></li>
+                        <li><a href="#"><i class="fa fa-phone"></i>Telefon</a></li>
+                    </ul>
+                </li> 
             </ul>
         </div>
 
         <div class="navbar__nav-secondary">
             <ul class="navbar__nav-secondary__menue">
-                <li><a href="#kundenformulare">
-                    {{-- TODO: Eigene Componente: Runder Icon Button --}}
-                    Kundenformulare
-                </a></li>
+                <li>
+                    <a href="#kundenformulare"><i class="fa fa-file-text-o"></i></a>
+                </li>
             </ul>
-        </div>
-
-
-        
-
+        </div>  
 
     </nav>
 
