d6261be4e11c7348a736cdc531929dda82c06a49
Patrick Kernke <patrick@kernke.net>
2015-10-12 10:56:59 +0200

    Clients styling

---
+++ resources/views/partials/clients.blade.php
@@ -0,0 +1,20 @@
+<section class="Clients">
+    <div class="Clients__client">
+        <img src="/images/clients/client-logos_canyon.png">
+    </div>
+    <div class="Clients__client">
+        <img src="/images/clients/client-logos_IKEA.png">
+    </div>
+    <div class="Clients__client">
+        <img src="/images/clients/client-logos_Jungbluth.png">
+    </div>
+    <div class="Clients__client">
+        <img src="/images/clients/client-logos_Mercedes.png">
+    </div>
+    <div class="Clients__client">
+        <img src="/images/clients/client-logos_Stabilus.png">
+    </div>
+    <div class="Clients__client">
+        <img src="/images/clients/client-logos_TNT.png">
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,27 @@
 
     @include('partials.slider')
 
+    <section class="Clients">
+        <div class="Clients__client">
+            <img src="/images/clients/client-logos_canyon.png">
+        </div>
+        <div class="Clients__client">
+            <img src="/images/clients/client-logos_IKEA.png">
+        </div>
+        <div class="Clients__client">
+            <img src="/images/clients/client-logos_Jungbluth.png">
+        </div>
+        <div class="Clients__client">
+            <img src="/images/clients/client-logos_Mercedes.png">
+        </div>
+        <div class="Clients__client">
+            <img src="/images/clients/client-logos_Stabilus.png">
+        </div>
+        <div class="Clients__client">
+            <img src="/images/clients/client-logos_TNT.png">
+        </div>
+    </section>
+
     
 </body>
 </html>
