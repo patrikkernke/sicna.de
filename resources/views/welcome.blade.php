210ae617107c17d06add8aebb11d906b74f835e0
Patrick Kernke <patrick@kernke.net>
2015-10-12 12:47:34 +0200

    Teaser styling: styling & advert

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,15 @@
 
     @include('partials.clients')
 
+    <section class="Teaser">
+        <div class="Filmhero"></div>
+        <div class="Teaser__body">
+            <h2>Styling oder Werbung?</h2>
+            <p>Dann sind wir der richtige Partner. Egal ob Entwicklung und Umsetzung Ihrer Corporate Identity, Realisierung und Wartung von Fahrzeugflotten, Werbebeschriftungen, Car-Wrappings oder Verschönerung des Fahrzeuglooks: in allen Bereichen besitzen wir über 30 Jahre Erfahrungen und können Sie optimal beraten.</p>
+            <a href="#" class="Button">Mehr Informationen</a>
+        </div>
+    </section>
+
     
 </body>
 </html>
