69d10e9c528b93aae0ce57886605d90e3cb19f27
Patrick Kernke <patrick@kernke.net>
2015-10-12 11:08:26 +0200

    Bugfix templating

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,26 +13,7 @@
 
     @include('partials.slider')
 
-    <section class="Clients">
-        <div class="Clients__client">
-            <img src="/images/clients/client-logos_canyon.png">
-        </div>
-        <div class="Clients__client">
-            <img src="/images/clients/client-logos_IKEA.png">
-        </div>
-        <div class="Clients__client">
-            <img src="/images/clients/client-logos_Jungbluth.png">
-        </div>
-        <div class="Clients__client">
-            <img src="/images/clients/client-logos_Mercedes.png">
-        </div>
-        <div class="Clients__client">
-            <img src="/images/clients/client-logos_Stabilus.png">
-        </div>
-        <div class="Clients__client">
-            <img src="/images/clients/client-logos_TNT.png">
-        </div>
-    </section>
+    @include('partials.clients')
 
     
 </body>
