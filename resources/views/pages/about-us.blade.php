818b1463fb340e24d681085b5faae8bf6fa70b26
Patrick Kernke <patrick@kernke.net>
2015-10-23 15:28:32 +0200

    aboutus-page styling (wip=

--- resources/views/pages/about-us.blade.php
+++ resources/views/pages/about-us.blade.php
@@ -1,16 +1,29 @@
 @extends('layouts.app')
 
 @section('app.content')
-    @include('layouts.partials.hero')
-    @include('layouts.partials.product.description')
-@stop
-
-@section('hero.title', 'Über uns')
-@section('hero.image-path', '/images/hero/sun-protection_car.jpg')
-
-@section('product.description')
     
-    <p><stroing>SICNA.de</stroing> ist ein Familienbetrieb der auf <strong>30 Jahre Materialerfahrung</strong> zurückblicken kann. Wir legen viel Wert auf eine umfassende <strong>fachkompetente Beratung</strong>. Jeder Auftrag muss hinsichtlich Anwendungszweck, Material, Haltbarkeit und Preis neu analysiert werden. Wir sehen uns als Partner unserer Kunden und suchen für Sie langfristige Lösungen. Dafür nutzen wir bewährte Produkte unserer Lieferanten und scheuen uns nicht davor in Zusammenarbeit <strong>individuelle und einzigartige Lösungen</strong> zu finden. 
-</p>
+    @include('partials.hero-map', ['hero_title' => 'Über uns'])
+    
+    <section class="Product">
+        <p><stroing>SICNA.de</stroing> ist ein Familienbetrieb der auf <strong>30 Jahre Materialerfahrung</strong> zurückblicken kann. Wir legen viel Wert auf eine umfassende <strong>fachkompetente Beratung</strong>. Jeder Auftrag muss hinsichtlich Anwendungszweck, Material, Haltbarkeit und Preis neu analysiert werden. Wir sehen uns als Partner unserer Kunden und suchen für Sie langfristige Lösungen. Dafür nutzen wir bewährte Produkte unserer Lieferanten und scheuen uns nicht davor in Zusammenarbeit <strong>individuelle und einzigartige Lösungen</strong> zu finden.</p>
 
-@stop
+        <img src="/images/sicna-logo_nav.png">
+
+        <h5>SICNA.de (Inh. Gerhard Reichert)</h5>
+        <h6>Mailadresse</h6>
+        <p>info@sicna.de</p>
+        <h6>Telefon</h6>
+        <p>+49 (0) 2630 - 84088</p>
+        <h6>Adresse</h6>
+        <p>Rudolf-Diesel-Straße 8 <br> 
+        56220 Urmitz bei Koblenz</p>
+    </section>
+
+    <section class="Partners">
+        <img src="/images/partners/logo-hexis.png">
+        <img src="/images/partners/logo-avery.png">
+        <img src="/images/partners/logo-3m.png">
+        <img src="/images/partners/logo-orafol.png">
+    </section>
+
+@endsection
---
+++ resources/views/partials/hero-map.blade.php
@@ -0,0 +1,13 @@
+<section class="Hero">
+
+    <div class="Hero__caption">
+        <h1>{{ $hero_title or 'Seitentitel' }}</h1>
+    </div>
+
+    <div class="Hero__map">
+        <iframe width='100%' height='500px' frameBorder='0' src='https://a.tiles.mapbox.com/v4/pake.hmk2f5kl/attribution.html?access_token=pk.eyJ1IjoicGFrZSIsImEiOiJyUzdfRC1FIn0.AFP5O4J9g6niNRzhpRctFg'></iframe>
+    </div>
+
+    <div class="Hero__filter"></div>
+
+</section>
